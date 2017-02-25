e721175aa8e0c019b4d8c879fa3cb9d15a8037e4
Steven Galvin <steven.john.galvin@gmail.com>
2017-02-24 22:28:20 -0800

    add a few line breaks on javascript to make reading the code easier.

--- js/scripts.js
+++ js/scripts.js
@@ -13,10 +13,15 @@
 $(document).ready(function() {
   $("form.quiz").submit(function(event) {
     var answer1 = parseInt($('input:radio[name=question1]:checked').val());
+
     var answer2 = parseInt($('input:radio[name=question2]:checked').val());
+
     var answer3 = parseInt($('input:radio[name=question3]:checked').val());
+
     var answer4 = parseInt($('input:radio[name=question4]:checked').val());
+
     var answer5 = parseInt($('input:radio[name=question5]:checked').val());
+
     var userScore = (answer1 + answer2 + answer3 + answer4 + answer5)
 
     var firstName = $("input#first-name").val();
